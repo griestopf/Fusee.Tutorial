03e069ab72bb75a578081d45cef19efb97e4b1a7
Christoph Mueller <chrism at g-graphix.de>
2016-03-20 18:03:13 +0100

    Tutorial01 added

---
+++ Tutorial01Completed/Core/Tutorial.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Fusee.Base.Common;
+using Fusee.Base.Core;
+using Fusee.Engine.Common;
+using Fusee.Engine.Core;
+using Fusee.Engine.Core.GUI;
+using Fusee.Math.Core;
+using Fusee.Serialization;
+
+
+namespace Fusee.Tutorial.Core
+{
+
+    [FuseeApplication(Name = "Tutorial Example", Description = "The official FUSEE Tutorial.")]
+    public class Tutorial : RenderCanvas
+    {
+        private Mesh _mesh;
+        private const string _vertexShader = @"
+            attribute vec3 fuVertex;
+
+            void main()
+            {
+                gl_Position = vec4(fuVertex, 1.0);
+            }";
+
+        private const string _pixelShader = @"
+            #ifdef GL_ES
+                precision highp float;
+            #endif
+
+            void main()
+            {
+                gl_FragColor = vec4(1, 0, 1, 1);
+            }";
+
+
+        // Init is called on startup. 
+        public override void Init()
+        {
+            _mesh = new Mesh
+            {
+                Vertices = new[]
+                {
+                    new float3(-0.75f, -0.75f, 0),
+                    new float3(0.75f, -0.75f, 0),
+                    new float3(0, 0.75f, 0),
+                },
+                Triangles = new ushort[] {0, 1, 2},
+            };
+
+            var shader = RC.CreateShader(_vertexShader, _pixelShader);
+            RC.SetShader(shader);
+
+            // Set the clear color for the backbuffer to white (100% intentsity in all color channels R, G, B, A).
+            RC.ClearColor = new float4(0.5f, 1, 0.7f, 1);
+        }
+
+        // RenderAFrame is called once a frame
+        public override void RenderAFrame()
+        {
+            // Clear the backbuffer
+            RC.Clear(ClearFlags.Color | ClearFlags.Depth);
+
+            RC.Render(_mesh);
+
+            // Swap buffers: Show the contents of the backbuffer (containing the currently rerndered farame) on the front buffer.
+            Present();
+        }
+
+
+        // Is called when the window was resized
+        public override void Resize()
+        {
+            // Set the new rendering area to the entire new windows size
+            RC.Viewport(0, 0, Width, Height);
+
+            // Create a new projection matrix generating undistorted images on the new aspect ratio.
+            var aspectRatio = Width/(float) Height;
+
+            // 0.25*PI Rad -> 45° Opening angle along the vertical direction. Horizontal opening angle is calculated based on the aspect ratio
+            // Front clipping happens at 1 (Objects nearer than 1 world unit get clipped)
+            // Back clipping happens at 2000 (Anything further away from the camera than 2000 world units gets clipped, polygons will be cut)
+            var projection = float4x4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 1, 20000);
+            RC.Projection = projection;
+        }
+
+    }
+}
